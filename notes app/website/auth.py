ff5e3a3ca3989828304b14cdf08f669113601735
Snehal Adbol <smitaadbol01@gmail.com>
2021-12-20 00:26:44 +0530

    initial commit

---
+++ notes app/website/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/signup')
+def signup():
+    return "<p>Signup</p>"
