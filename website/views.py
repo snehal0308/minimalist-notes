0dd4f957240363275888d419531b2c1c4b7d515c
Snehal Adbol <smitaadbol01@gmail.com>
2021-12-20 13:00:43 +0530

    files added

---
+++ website/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def home():
+    return render_template("index.html")
+
+
+@views.route('/contact')
+def contact ():
+    return render_template('contact.html')
+
+
+
+
